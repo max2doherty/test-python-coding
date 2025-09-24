d8ff40d33827657e30b0b13506806d2484db0b17
max2doherty <max.doherty@sportsbet.com.au>
2025-09-24 12:01:33 +1000

    Completed Task 0: Square with size

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialize the square with a given size.
+
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.__size = size
