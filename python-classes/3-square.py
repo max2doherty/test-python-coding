4009cfecf3b214050ef52ad610a1e5fac9f7f4be
max2doherty <max.doherty@sportsbet.com.au>
2025-09-24 13:55:02 +1000

    Completed Task 4: Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
     def size(self):
         """Get the size of the square."""
         return self.__size
-   
+
     @size.setter
     def size(self, value):
         """Set the size of the square with validation.
